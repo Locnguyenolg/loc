8007c72150b42e030e48b339edc3c996cad87d77
Locnguyenolg <59641863+Locnguyenolg@users.noreply.github.com>
2026-01-19 15:34:24 +0700

    Update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -34,7 +34,7 @@ export default defineConfig({
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
     launchOptions: {
-      headless: true,
+      headless: false,
       slowMo: 300,
     },
   },
